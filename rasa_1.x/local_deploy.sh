33161713bf5b80975f58880245be5cac6660f377
sfh@ubu <l.lassus@gmail.com>
2020-11-21 19:58:34 +0100

    adds nodejs & markdown link extractor packages installations

--- rasa_1.x/local_deploy.sh
+++ rasa_1.x/local_deploy.sh
@@ -1,6 +1,8 @@
 pip install -r requirements.txt
 pip install python-dateutil==2.8
 pip install rasa-x --extra-index-url https://pypi.rasa.com/simple
+pip install nodejs
+npm install --save markdown-link-extractor
 python -m spacy download en
 python -m spacy download fr
 rasa train
